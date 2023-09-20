c5cae654e1daf61c33a79296f32b76a2f4fb8ef5
Francesco Frassinelli <francesco.frassinelli@nina.no>
2023-09-20 23:10:54 +0200

    Implement simple import/export

---
+++ pyopenrefine/__init__.py
@@ -0,0 +1,109 @@
+import json
+import logging
+from urllib.parse import parse_qs, urlparse
+
+import requests
+
+DEFAULT_OPTIONS = {
+    "encoding": "UTF-8",
+    "separator": ",",
+    "ignoreLines": -1,
+    "headerLines": 1,
+    "skipDataLines": 0,
+    "limit": -1,
+    "storeBlankRows": True,
+    "guessCellValueTypes": False,
+    "processQuotes": True,
+    "quoteCharacter": '"',
+    "storeBlankCellsAsNulls": True,
+    "includeFileSources": False,
+    "includeArchiveFileName": False,
+    "trimStrings": False,
+    "disableAutoPreview": False,
+}
+
+
+class OpenRefine:
+    def __init__(self, url, public_url):
+        self.url = url
+        self.public_url = public_url
+        self.csrf_token = None
+        self.update_csrf_token()
+
+    def update_csrf_token(self):
+        response = self.request("get-csrf-token")
+        parsed = response.json()
+        self.csrf_token = parsed["token"]
+
+    def request(self, command, method="GET", files=None, **params):
+        if self.csrf_token:
+            params["csrf_token"] = self.csrf_token
+        response = requests.request(
+            method,
+            self.url + "/command/core/" + command,
+            files=files,
+            params=params,
+            stream=True,
+        )
+        response.raise_for_status()
+        return response
+
+    def create_project(self, name, files, file_format, index=0, **options):
+        all_options = DEFAULT_OPTIONS
+        all_options.update(options)
+
+        all_options["sheets"] = []
+        form = []
+        # https://requests.readthedocs.io/en/latest/user/advanced/#post-multiple-multipart-encoded-files
+        for file in files:
+            form.append(
+                (
+                    "project-file",
+                    (
+                        file["filename"],
+                        file["content"],
+                        file["mime_type"],
+                    ),
+                )
+            )
+            sheet = {"fileNameAndSheetIndex": file["filename"] + "#%d" % index}
+            all_options["sheets"].append(sheet)
+        form.extend(
+            [
+                ("project-name", (None, name)),
+                ("format", (None, file_format)),
+                ("options", (None, json.dumps(all_options))),
+            ]
+        )
+
+        response = self.request("create-project-from-upload", method="POST", files=form)
+
+        project = int(parse_qs(urlparse(response.url).query)["project"][0])
+
+        # https://github.com/OpenRefine/OpenRefine/issues/5387
+        self.get_rows(project)
+
+        return project
+
+    def get_rows(self, project, start=0, limit=0):
+        response = self.request("get-rows", project=project, start=start, limit=limit)
+        return response.json()
+
+    def project_url(self, project):
+        internal_url = requests.get(
+            self.url + "/project", params={"project": project}
+        ).url
+        if not internal_url.startswith(self.url):
+            logging.error(
+                f"{self.url} differs from the URL of the project {internal_url}"
+            )
+            return internal_url
+        return internal_url.replace(self.url, self.public_url, 1)
+
+    def project_export(self, project, file_format="csv"):
+        return self.request(
+            "export-rows/csv",
+            method="POST",
+            project=project,
+            format=file_format,
+        )
--- tests/test_openrefine.py
+++ tests/test_openrefine.py
@@ -1,23 +1,79 @@
+import re
 from datetime import timedelta
+from textwrap import dedent
+from time import time
 
 import pytest
-from pytest_container import Container
-from pytest_container.container import ContainerData
-from pytest_container.inspect import ContainerHealth
+from pytest_container import DerivedContainer
+from pytest_container.container import ContainerBase, ContainerData
+from pytest_container.inspect import ContainerHealth, PortForwarding
+
+from pyopenrefine import OpenRefine
 
 TAGS = ("master", "4.0", "3.7", "3.6.2")
 IMAGES = [
-    Container(
-        url=f"ghcr.io/ninanor/openrefine:{tag}",
-        healthcheck_timeout=timedelta(seconds=60),
+    DerivedContainer(
+        base=f"ghcr.io/ninanor/openrefine:{tag}",
+        containerfile=dedent(
+            """
+        HEALTHCHECK --interval=1s --start-period=10s CMD curl -sSf -o /dev/null http://localhost:3333
+        """
+        ),
+        forwarded_ports=[PortForwarding(container_port=3333)],
+        healthcheck_timeout=timedelta(seconds=30),
     )
     for tag in TAGS
 ]
 
+csv_file = {
+    "filename": "test.csv",
+    "content": b"col1,col2\n1,2\n3,4\n",
+    "mime_type": "text/line-based/*sv",
+    "last_modified": time(),
+}
 
-@pytest.mark.parametrize("container", IMAGES, indirect=["container"])
-def test_health(container: ContainerData, container_runtime):
+
+@pytest.mark.parametrize("container", IMAGES, indirect=True)
+def test_health(container: ContainerData, container_runtime: ContainerBase):
     assert (
         container_runtime.get_container_health(container.container_id)
         == ContainerHealth.HEALTHY
     )
+
+
+@pytest.mark.parametrize("container", IMAGES, indirect=True)
+def test_csrf(container: ContainerData, container_runtime: ContainerBase):
+    port = container.forwarded_ports[0].host_port
+    url = f"http://localhost:{port}"
+    openrefine = OpenRefine(url, url)
+    assert len(openrefine.csrf_token) == 32
+
+
+@pytest.mark.parametrize("container", IMAGES, indirect=True)
+def test_create_project(container: ContainerData, container_runtime: ContainerBase):
+    port = container.forwarded_ports[0].host_port
+    url = f"http://localhost:{port}"
+    openrefine = OpenRefine(url, url)
+    project = openrefine.create_project(
+        "test",
+        [csv_file],
+        csv_file["mime_type"],
+    )
+    assert isinstance(project, int)
+    assert 0 < project
+    project_url = openrefine.project_url(project)
+    assert re.match(r"^%s/project\?project=\d{13}$" % url, project_url)
+
+
+@pytest.mark.parametrize("container", IMAGES, indirect=True)
+def test_upload_download(container: ContainerData, container_runtime: ContainerBase):
+    port = container.forwarded_ports[0].host_port
+    url = f"http://localhost:{port}"
+    openrefine = OpenRefine(url, url)
+    project = openrefine.create_project(
+        "test",
+        [csv_file],
+        csv_file["mime_type"],
+    )
+    export = openrefine.project_export(project)
+    assert export.content == csv_file["content"]
